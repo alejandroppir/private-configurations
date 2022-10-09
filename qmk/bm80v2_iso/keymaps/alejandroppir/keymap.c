e34f9f0e6b42ec228d7899d5f9f83f50b2c58102
Alejandro Picos Prieto <picosprietoalejandro@gmail.com>
2022-10-09 23:58:55 +0200

    Update keymap.c

--- qmk/bm80v2_iso/keymaps/alejandroppir/keymap.c
+++ qmk/bm80v2_iso/keymaps/alejandroppir/keymap.c
@@ -5,6 +5,16 @@
 #include "quantum/keymap_extras/keymap_us_international.h"
 #include "features/custom_keys.h"
 
+// Tap Dance declarations
+enum tap_dance{
+    TD_LSFT_CAPS,
+    TD_RSFT_CAPS,
+};
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
+};
+
 enum layers {
     _CST, // Custom layout
     _LAB, // Labeled layout
@@ -19,8 +29,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, KC_PAUS,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     ES_SLSH,  ES_PLUS, KC_BSPC,    KC_DEL,  KC_PGUP, KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     ES_LBRC,  ES_RBRC,             KC_INS,  KC_PGDN, KC_END,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    ES_NTIL,  ES_QUOT,  ES_SCLN, KC_ENT,
-        KC_LSFT,TG(_UTL), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   ES_MINS,           KC_RSFT,             KC_UP,
+        ES_ACUT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    ES_NTIL,  ES_QUOT,  ES_SCLN, KC_ENT,
+        TD(TD_LSFT_CAPS),TG(_UTL), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   ES_MINS,           TD(TD_RSFT_CAPS),             KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT,  MO(_UTL), KC_APP , KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
