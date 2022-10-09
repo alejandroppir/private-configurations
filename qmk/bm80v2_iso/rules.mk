20ce316170d71f96a32bfcd38ca4f6fb788c2f48
Alejandro Picos Prieto <picosprietoalejandro@gmail.com>
2022-10-09 23:59:04 +0200

    Update rules.mk

--- qmk/bm80v2_iso/rules.mk
+++ qmk/bm80v2_iso/rules.mk
@@ -17,6 +17,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 KEY_OVERRIDE_ENABLE = yes   # Override key
+TAP_DANCE_ENABLE = yes
 # CONSOLE_ENABLE = yes
 
 RGB_MATRIX_ENABLE = yes         # Use RGB matrix
